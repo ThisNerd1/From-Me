085d9a9a6b7b4adb0b4188b970f9ef55693f1455
Daijah Coppage <89088157+ThisNerd1@users.noreply.github.com>
2026-07-23 21:43:56 -0600

    notes to self

--- Frontend/from-me/src/App.jsx
+++ Frontend/from-me/src/App.jsx
@@ -6,8 +6,27 @@ function App() {
   return (
     <>
       <h1>From me, to you...</h1>
+      <ul>List todo:
+        Navbar - Home, link2, link3, link4
+        <br/>
+        <li>Four themes;</li>
+        <ol>Green Apple Pie Theme</ol>
+        <ol>Blueberry Pie Theme</ol>
+        <ol>Apricot Pie Theme</ol>
+        <ol>Lemon Pie Theme</ol>
+      </ul>
+      <ul>Make pie svg in Canva</ul>
+      <li>Feature - </li>
+      <ul>Turn off pie svg in profile settings</ul>
+      <ul>Could then offer other color schemes potenially</ul>
+      <h2>Make cards of categories, inside those cards, have sub-categories</h2>
+      <p>Have them clickable and ability to go back</p>
     </>
   )
 }
 
 export default App
+// #A8E4A0 green apple
+// #4f86f7 blueberry
+// #F6E199 lemon
+// #F3A76B apricot
