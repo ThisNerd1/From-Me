6e0b3d0b0639a29853e882793536b556ea9f5c5d
Daijah Coppage <89088157+ThisNerd1@users.noreply.github.com>
2026-09-05 19:34:44 -0600

    update

--- Frontend/from-me/src/Components/SubCategory.jsx
+++ Frontend/from-me/src/Components/SubCategory.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Card, Row, Col, Container } from "react-bootstrap";
 import Navbar from './Navbar';
 import { useNavigate } from "react-router";
 import { useLocation } from "react-router-dom";
 import Data from '../Api/data.json';
 
 
-function SubCategory({selected}) {
+function SubCategory() {
   const [currentColor, setCurrentColor] = useState('transparent');
   let navigate = useNavigate();
   const location = useLocation();
@@ -28,18 +28,21 @@ function SubCategory({selected}) {
     <Button onClick={() => navigate("/")} variant="primary">Back</Button>
     </div>
     <br/>
-    <div className="d-flex justify-content-center gap-3">
-    <Card style={{ width: '18rem' }}>
-    {specificData.map((i) => {
-      return ( 
-      <Card.Body key={i.id}>
-        <Card.Title>{i.title}</Card.Title>
-        <Card.Text>{i.text}</Card.Text>
-      </Card.Body>)
-    })}
-    </Card>
+    <Container className='d-flex vh-100 justify-content-center'>
+      <Row>
+        {specificData.map((item, k) => (
+            <Col key={k}>
+                <Card style={{ width: '18rem'}}>
+                    <Card.Body>
+                        <Card.Title>{item.title}</Card.Title>
+                        <Card.Text>{item.text}</Card.Text>
+                    </Card.Body>
+                  </Card>
+              </Col>
+            ))}
+        </Row>
+    </Container>
     </div>
-  </div>
   </>
   )
 };
